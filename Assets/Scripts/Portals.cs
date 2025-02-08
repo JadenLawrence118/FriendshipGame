f33e7f7bc79a6c7ba4c0620f15f1cafca6404e0d
Jaden Lawrence <jaden2.lawrence@live.uwe.ac.uk>
2025-02-08 22:44:02 +0000

    Added portals
    Added portals that teleport a player between 2 points. The portals have a sound effect, and a cooldown after use. Also added the template for puzzle 8

---
+++ Assets/Scripts/Portals.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Portals : MonoBehaviour
+{
+    private GameObject partner;
+    [SerializeField] private float cooldown = 3.0f;
+    private void Awake()
+    {
+        if (transform.GetSiblingIndex() == 0)
+        {
+            partner = transform.parent.transform.GetChild(transform.GetSiblingIndex() + 1).gameObject;
+        }
+        else
+        {
+            partner = transform.parent.transform.GetChild(transform.GetSiblingIndex() - 1).gameObject;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.gameObject.transform.position = new Vector2(partner.transform.position.x, partner.transform.position.y);
+        GetComponent<CapsuleCollider2D>().enabled = false;
+        partner.GetComponent<CapsuleCollider2D>().enabled = false;
+        GetComponent<SpriteRenderer>().color = new Color(GetComponent<SpriteRenderer>().color.r, GetComponent<SpriteRenderer>().color.g, GetComponent<SpriteRenderer>().color.b, 0.5f);
+        partner.GetComponent<SpriteRenderer>().color = new Color(partner.GetComponent<SpriteRenderer>().color.r, partner.GetComponent<SpriteRenderer>().color.g, partner.GetComponent<SpriteRenderer>().color.b, 0.5f);
+        transform.GetChild(0).gameObject.SetActive(false);
+        partner.transform.GetChild(0).gameObject.SetActive(false);
+        GetComponent<AudioSource>().Play();
+        StartCoroutine(Wait());
+    }
+    IEnumerator Wait()
+    {
+        yield return new WaitForSecondsRealtime(cooldown);
+        GetComponent<CapsuleCollider2D>().enabled = true;
+        partner.GetComponent<CapsuleCollider2D>().enabled = true;
+        GetComponent<SpriteRenderer>().color = new Color(GetComponent<SpriteRenderer>().color.r, GetComponent<SpriteRenderer>().color.g, GetComponent<SpriteRenderer>().color.b, 1.0f);
+        partner.GetComponent<SpriteRenderer>().color = new Color(partner.GetComponent<SpriteRenderer>().color.r, partner.GetComponent<SpriteRenderer>().color.g, partner.GetComponent<SpriteRenderer>().color.b, 1.0f);
+        transform.GetChild(0).gameObject.SetActive(true);
+        partner.transform.GetChild(0).gameObject.SetActive(true);
+    }
+}
