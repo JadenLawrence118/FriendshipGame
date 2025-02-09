9a69c307aa617919af5bc73570a2040ae144749d
Jaden Lawrence <jaden2.lawrence@live.uwe.ac.uk>
2025-02-09 21:49:44 +0000

    Added puzzle 9
    Added puzzle 9, combining portals and timed doors. Also made a minor change to portals adding the ability to have them ignore specified objects. These objects can be added to the ignore list in the editor

--- Assets/Scripts/Portals.cs
+++ Assets/Scripts/Portals.cs
@@ -7,6 +7,7 @@ public class Portals : MonoBehaviour
 {
     private GameObject partner;
     [SerializeField] private float cooldown = 3.0f;
+    [SerializeField] private GameObject[] ignoreList;
     private void Awake()
     {
         if (transform.GetSiblingIndex() == 0)
@@ -20,17 +21,28 @@ public class Portals : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        collision.gameObject.transform.position = new Vector2(partner.transform.position.x, partner.transform.position.y);
-        GetComponent<CapsuleCollider2D>().enabled = false;
-        partner.GetComponent<CapsuleCollider2D>().enabled = false;
-        GetComponent<SpriteRenderer>().color = new Color(GetComponent<SpriteRenderer>().color.r, GetComponent<SpriteRenderer>().color.g, GetComponent<SpriteRenderer>().color.b, 0.5f);
-        partner.GetComponent<SpriteRenderer>().color = new Color(partner.GetComponent<SpriteRenderer>().color.r, partner.GetComponent<SpriteRenderer>().color.g, partner.GetComponent<SpriteRenderer>().color.b, 0.5f);
-        transform.GetChild(0).gameObject.SetActive(false);
-        partner.transform.GetChild(0).gameObject.SetActive(false);
-        GetComponent<AudioSource>().Play();
-        StartCoroutine(Wait());
+        bool teleport = true;
+        for (int i = 0; i < ignoreList.Length; i++)
+        {
+            if (collision.gameObject == ignoreList[i])
+            {
+                teleport = false;
+            }
+        }
+        if (teleport)
+        {
+            collision.gameObject.transform.position = new Vector2(partner.transform.position.x, partner.transform.position.y);
+            GetComponent<CapsuleCollider2D>().enabled = false;
+            partner.GetComponent<CapsuleCollider2D>().enabled = false;
+            GetComponent<SpriteRenderer>().color = new Color(GetComponent<SpriteRenderer>().color.r, GetComponent<SpriteRenderer>().color.g, GetComponent<SpriteRenderer>().color.b, 0.5f);
+            partner.GetComponent<SpriteRenderer>().color = new Color(partner.GetComponent<SpriteRenderer>().color.r, partner.GetComponent<SpriteRenderer>().color.g, partner.GetComponent<SpriteRenderer>().color.b, 0.5f);
+            transform.GetChild(0).gameObject.SetActive(false);
+            partner.transform.GetChild(0).gameObject.SetActive(false);
+            GetComponent<AudioSource>().Play();
+            StartCoroutine(Cooldown());
+        }
     }
-    IEnumerator Wait()
+    IEnumerator Cooldown()
     {
         yield return new WaitForSecondsRealtime(cooldown);
         GetComponent<CapsuleCollider2D>().enabled = true;
